1ad98f9178d21a03107d956414f88f537f6d13a3
sanghaklee <code.ryan.lee@gmail.com>
2022-09-28 19:59:57 +0900

    feat: usage

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+php -S 0.0.0.0:8080 session.php
+```
--- session.php
+++ session.php
@@ -1,13 +1,12 @@
 <?php 
-    // Start the session
     // https://www.php.net/manual/en/session.configuration.php
     session_start([
         "cache_expire" => 1, // minutes
         'cookie_lifetime' => 60, // second
     ]);
 
-    // Set session variables
-    $_SESSION["name"]= "openstack";
+    $_SESSION["hostname"]= gethostname();
+
     echo json_encode(array(
         "session" => $_SESSION,
         "server" => $_SERVER["REMOTE_ADDR"]
